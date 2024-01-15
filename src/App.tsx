6f59ebac0bc844ee71c7e8dc7501a9353f1b5648
mikejeffers <mike.s.jeffers@gmail.com>
2024-01-14 22:36:43 -0500

    add ui framework, mock up chat window

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
+//@ts-check
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppShell, Burger, Button, Stack, Grid } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import Chat from './Chat';
 
-function App() {
+const App = ( ) => {
+  const [opened, { toggle }] = useDisclosure();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppShell
+      padding="md"
+      offsetScrollbars={false}
+      header={{ height: 64 }}
+      navbar={{
+        width: 120,
+        breakpoint: 'xs',
+        collapsed: { desktop: !opened, mobile: !opened }
+      }}>
+      <AppShell.Header p="xs" style={{ height: 64 }}>
+        <Grid>
+          <Grid.Col span={1}>
+            <Burger opened={opened} onClick={toggle} />
+          </Grid.Col>
+          <Grid.Col span={9}>
+          </Grid.Col>
+        </Grid>
+      </AppShell.Header>
+      <AppShell.Navbar hidden={!opened} p="xs">
+        <Stack>
+        </Stack>
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Chat></Chat>
+      </AppShell.Main>
+    </AppShell>
+  )
 }
 
 export default App;
---
+++ src/Chat.tsx
@@ -0,0 +1,31 @@
+//@ts-check
+import React from 'react';
+import { AppShell, Burger, Button, Grid, Stack, Table } from '@mantine/core';
+
+const chatRow = (message: string, from: string, at: Date) => {
+  return (<Table.Tr>
+    <Table.Td>{from}</Table.Td>
+    <Table.Td>{message}</Table.Td>
+    <Table.Td>{at.toISOString()}</Table.Td>
+  </Table.Tr>)
+}
+
+const Chat = () => {
+  const data = [
+    { message: 'hi', from: 'userA', at: new Date() },
+    { message: 'wow hi what is this', from: 'userB', at: new Date() },
+    { message: 'clearly some mock data', from: 'snarkyFella', at: new Date() },
+  ];
+  const rows = data.map(d => chatRow(d.message, d.from, d.at));
+  return (
+    <Grid justify="center" gutter={6} columns={12}>
+      <Grid.Col>
+        <Table>
+          {rows}
+        </Table>
+      </Grid.Col>
+    </Grid>
+  )
+};
+
+export default Chat;
